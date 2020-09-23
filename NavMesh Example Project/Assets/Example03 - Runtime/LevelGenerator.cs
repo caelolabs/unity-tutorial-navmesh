6ddcf28e70a1c9f7eec60195970ee7a4e8213fc8
jaredbest <me@jaredbest.com>
2020-09-23 16:11:01 +0200

    update tutorial files

--- NavMesh Example Project/Assets/Example03 - Runtime/LevelGenerator.cs
+++ NavMesh Example Project/Assets/Example03 - Runtime/LevelGenerator.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LevelGenerator : MonoBehaviour {
+
+	public NavMeshSurface surface;
 
 	public int width = 10;
 	public int height = 10;
@@ -12,28 +15,27 @@ public class LevelGenerator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GenerateLevel();
+		GenerateLevel ();
+
+		// Update NavMesh
+		surface.BuildNavMesh ();
 	}
-	
+
 	// Create a grid based level
-	void GenerateLevel()
-	{
+	void GenerateLevel () {
 		// Loop over the grid
-		for (int x = 0; x <= width; x+=2)
-		{
-			for (int y = 0; y <= height; y+=2)
-			{
+		for (int x = 0; x <= width; x += 2) {
+			for (int y = 0; y <= height; y += 2) {
 				// Should we place a wall?
-				if (Random.value > .7f)
-				{
+				if (Random.value >.7f) {
 					// Spawn a wall
-					Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
-					Instantiate(wall, pos, Quaternion.identity, transform);
+					Vector3 pos = new Vector3 (x - width / 2f, 1f, y - height / 2f);
+					Instantiate (wall, pos, Quaternion.identity, transform);
 				} else if (!playerSpawned) // Should we spawn a player?
 				{
 					// Spawn the player
-					Vector3 pos = new Vector3(x - width / 2f, 1.25f, y - height / 2f);
-					Instantiate(player, pos, Quaternion.identity);
+					Vector3 pos = new Vector3 (x - width / 2f, 1.25f, y - height / 2f);
+					Instantiate (player, pos, Quaternion.identity);
 					playerSpawned = true;
 				}
 			}
